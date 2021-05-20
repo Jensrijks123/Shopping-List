186c1f709fb1bb38fe7fee3cc3982bb36b96e168
Jens Rijks <jens.rijks@student.hu.nl>
2021-05-20 23:53:16 +0200

    Sprint 1

---
+++ public/src/Boodschappenlijstje.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Boodschappenlijstje {
+    
+    public String lijsteNaam;
+    public ArrayList<Item> items;
+    public ArrayList<User> users;
+
+}
---
+++ public/src/Item.java
@@ -0,0 +1,6 @@
+import jdk.tools.jlink.resources.plugins;
+
+public class Item {
+    public String itemNaam;
+
+}
